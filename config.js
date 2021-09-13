1d358250e2dfc665a83daa2c31d51a36a0f8a72f
Bintangp02 <80300921+Bintangp02@users.noreply.github.com>
2021-09-13 19:57:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089', '6281334177340'] // Put your number here
+global.owner = ['6285647417373'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -20,8 +20,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.packname = 'Bintangp'
+global.author = '02'
 
 global.multiplier = 69 // The higher, The harder levelup
 
